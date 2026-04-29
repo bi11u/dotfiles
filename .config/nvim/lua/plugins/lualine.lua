06781c54efe4611fe28153cee298e36172c4be82
bi11u <273764439+bi11u@users.noreply.github.com>
2026-04-29 17:41:46 +0530

    update and fix the configs

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -8,8 +8,8 @@ return {
 			options = {
 				icons_enabled = true,
 				theme = "gruvbox-material",
-				section_separators = { left = "", right = "" },
-				component_separators = "|",
+				section_separators = { left = " ", right = " " },
+				component_separators = "",
 			},
 		})
 	end,
